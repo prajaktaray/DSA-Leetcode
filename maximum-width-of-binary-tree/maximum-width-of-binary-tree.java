04ef89a4b572b2dc92f9af1ad5d3a907f076435f
Prajakta Ray <47615084+prajaktaray@users.noreply.github.com>
2021-03-08 23:08:08 -0600

    Time: 1 ms (91.77%), Space: 38.8 MB (38.61%) - LeetHub

--- maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -1,48 +1,45 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    
-    class Node{
-        TreeNode node;
-        int index;
-        Node(TreeNode node, int index){
-            this.node = node;
-            this.index = index;
-        }
-    }
-    public int widthOfBinaryTree(TreeNode root) {
-        int maxwidth =0;
-        if(root ==null) return maxwidth;
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(root,0));
-        int min=0;
-        int max=0;
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            
-            for(int i =0;i<size;i++){
-                Node curr = q.poll();
-                if(i==0) min = curr.index;
-                if(i==size-1) max = curr.index;
-                if(curr.node.left !=null) q.add(new Node(curr.node.left, 2*curr.index +1));
-                if(curr.node.right !=null) q.add(new Node(curr.node.right, 2*curr.index +2));
-            }
-            maxwidth = Math.max(maxwidth,(max-min+1));
-        }
-        return maxwidth;
-    }
+    class Node{
+        private TreeNode treenode;
+        private int index;
+        Node(TreeNode treenode,int index){
+            this.treenode =treenode;
+            this.index = index;
+        }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        int width=0;
+        if(root==null)return width;
+        Queue<Node> q= new LinkedList<>();
+        q.add(new Node(root,0));
+        int min=0,max=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            //int temp=0;
+            for(int i=0;i<size;i++){
+                Node curr = q.poll();
+                if(i==0)min=curr.index;
+                if(i==size-1) max=curr.index;
+                if(curr.treenode.left!=null) q.add(new Node(curr.treenode.left,curr.index*2+1));
+                if(curr.treenode.right!=null) q.add(new Node(curr.treenode.right,curr.index*2+2));
+            }
+            width=Math.max(width,(max-min+1));
+        }
+        return width;
+    }
 }
