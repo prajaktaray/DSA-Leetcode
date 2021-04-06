82094640f6e111ab0ad7352430c8512691e10083
Prajakta Ray <47615084+prajaktaray@users.noreply.github.com>
2021-04-05 21:41:45 -0500

    Time: 1 ms (98.49%), Space: 37.1 MB (67.79%) - LeetHub

--- valid-parentheses/valid-parentheses.java
+++ valid-parentheses/valid-parentheses.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()==1) return false;
-        Stack<Character> st=new Stack<>();
-        HashMap<Character,Character> map= new HashMap<>();
+        if(s == null || s.length() == 0)  return true;
+        HashMap<Character,Character> map = new HashMap<>();
         map.put(')','(');
-        map.put('}','{');
         map.put(']','[');
-        
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) ==')'||s.charAt(i) ==']'||s.charAt(i) =='}') {
-                if(st.isEmpty()) return false;
-                char c=st.pop();
-                if(c!=map.get(s.charAt(i))) return false;
-            }else{
-                st.add(s.charAt(i));
+        map.put('}','{');
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') st.add(s.charAt(i));
+            else{
+               // char ch = st.pop();
+                //if()
+                if(st.isEmpty() || st.pop() != map.get(s.charAt(i))) return false;
             }
         }
         
-        if(!st.isEmpty()) return false;
-        return true;
-        
+        return st.isEmpty();
     }
 }
